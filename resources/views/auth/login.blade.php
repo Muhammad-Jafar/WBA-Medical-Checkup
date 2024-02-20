0ec75bbeff27835c07cb0780f373b9016ec18a14
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-02-20 15:50:53 +0800

    add: / route to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 <x-guest-layout>
 
     <div id="auth-left">
-        <div class="auth-logo">
+        {{-- <div class="auth-logo">
             <a href="index.html"><img src="{{ asset('/images/logo/logo.png') }}" alt="Logo"></a>
-        </div>
-        <h1 class="auth-title">Log in.</h1>
-        <p class="auth-subtitle mb-5">Log in with your data that you entered during registration.</p>
+        </div> --}}
+        <h3 class="auth-title">Medical Checkup</h3>
+        <p class="auth-subtitle mb-5">BLUD RSUD Kab. Buton</p>
 
         @if (session('status'))
         <div class="mb-4 font-medium text-sm text-green-600">
@@ -33,15 +33,15 @@
                     <i class="bi bi-shield-lock"></i>
                 </div>
             </div>
-            <div class="form-check form-check-lg d-flex align-items-end">
+            {{-- <div class="form-check form-check-lg d-flex align-items-end">
                 <input class="form-check-input me-2" type="checkbox" name="remember" id="flexCheckDefault">
                 <label class="form-check-label text-gray-600" for="flexCheckDefault">
                     Keep me logged in
                 </label>
-            </div>
-            <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+            </div> --}}
+            <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Masuk sini</button>
         </form>
-        <div class="text-center mt-5 text-lg fs-4">
+        {{-- <div class="text-center mt-5 text-lg fs-4">
             @if (Route::has('register'))
             <p class="text-gray-600">Don't have an account? <a href="{{route('register')}}" class="font-bold">Sign
                     up</a>.</p>
@@ -51,6 +51,6 @@
             @if (Route::has('password.request'))
             <p><a class="font-bold" href="{{route('password.request')}}">Forgot password?</a>.</p>
             @endif
-        </div>
+        </div> --}}
     </div>
 </x-guest-layout>
