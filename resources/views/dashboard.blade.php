d234996c4a1fe712aee8c1542f7638037aadfbdd
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-02-21 16:33:29 +0800

    add: update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,22 +3,21 @@
         <div class="row">
             <div class="col-12 col-md-6 order-md-1 order-last">
                 <h3>Dashboard</h3>
-                {{-- <p class="text-subtitle text-muted">This is the main page.</p> --}}
+                <p class="text-subtitle text-muted">Rangkuman statistik :)</p>
             </div>
-            {{-- <div class="col-12 col-md-6 order-md-2 order-first">
+            <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
                     </ol>
                 </nav>
-            </div> --}}
+            </div>
         </div>
     </x-slot>
 
-
     <section class="section">
-        @livewire('dashboard.dashboard-menu')
-        <x-jet-section-border />
+        @include('dashboard.dashboard-menu')
+       
         {{-- <div class="card">
             <div class="card-header">
                 <h4 class="card-title">Example Content</h4>
--- resources/views/dashboard/dashboard-menu.blade.php
+++ resources/views/dashboard/dashboard-menu.blade.php
@@ -1,25 +1,26 @@
 	<div class="col-12 col-lg-12">
 		<div class="row">
 			<div class="col-6 col-lg-3 col-md-6">
-				<a href="{{ route('') }}">
-					<div class="card card-stat">
-						<div class="card-body px-3 py-4-5">
-							<div class="row">
-								<div class="col-md-4">
-									<div class="stats-icon purple">
-										<i class="iconly-boldProfile"></i>
-									</div>
-								</div>
-								<div class="col-md-8">
-									<h6 class="text-muted font-semibold">Pasien</h6>
-									{{-- <h6 class="font-extrabold {{ $studentCount <= 0 ? 'text-danger' : '' }} mb-0">
-										{{ $studentCount }}
-									</h6> --}}
-								</div>
-							</div>
-						</div>
-					</div>
-				</a>
+				<div class="card card-stat">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon purple">
+                                    <i class="iconly-boldProfile"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">Pasien</h6>
+                                {{-- <h6 class="font-extrabold {{ $studentCount <= 0 ? 'text-danger' : '' }} mb-0">
+                                    {{ $studentCount }}
+                                </h6> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <a href="{{ route('') }}">
+
+				</a> --}}
 			</div>
 			<div class="col-6 col-lg-3 col-md-6">
 				<div class="card card-stat">
@@ -60,23 +61,24 @@
 				</div>
 			</div>
 			<div class="col-6 col-lg-3 col-md-6">
-				<a href="{{ route('cash-transactions.index') }}">
-					<div class="card card-stat">
-						<div class="card-body px-3 py-4-5">
-							<div class="row">
-								<div class="col-md-4">
-									<div class="stats-icon red">
-										<i class="iconly-boldTicket"></i>
-									</div>
-								</div>
-								<div class="col-md-8">
-									<h6 class="text-muted font-semibold">SKBS 3 bulan terakhir</h6>
-									{{-- <h6 class="font-extrabold mb-0">{{ $amountThisMonth }}</h6> --}}
-								</div>
-							</div>
-						</div>
-					</div>
-				</a>
+				<div class="card card-stat">
+                    <div class="card-body px-3 py-4-5">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="stats-icon red">
+                                    <i class="iconly-boldTicket"></i>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h6 class="text-muted font-semibold">SKBS 3 bulan terakhir</h6>
+                                {{-- <h6 class="font-extrabold mb-0">{{ $amountThisMonth }}</h6> --}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- <a href="{{ route('cash-transactions.index') }}">
+
+				</a> --}}
 			</div>
 		</div>
 
@@ -155,4 +157,3 @@
 			</div>
 		</div> --}}
 	</div>
-    
