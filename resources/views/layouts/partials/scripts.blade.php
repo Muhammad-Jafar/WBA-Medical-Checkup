5f91580c5f2d15b3febe2adb24f771e5f8f6121b
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-02-22 23:52:14 +0800

    add: add datatables & jwt-auth

--- resources/views/layouts/partials/scripts.blade.php
+++ resources/views/layouts/partials/scripts.blade.php
@@ -3,7 +3,7 @@
 
 <script src="{{ asset('/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
 <script src="{{ asset('/vendors/tinymce/tinymce.min.js') }}"></script>
-
+<script src="{{ asset('/vendors/simple-datatables/simple-datatables.js') }}"></script>
 
 <script src="{{ asset('/js/bootstrap.bundle.min.js') }}"></script>
 
