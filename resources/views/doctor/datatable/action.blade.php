301d655e91c12692d2f4fd67537733da09ea722d
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-02-27 11:02:36 +0800

    add: add log for each needed table

--- resources/views/doctor/datatable/action.blade.php
+++ resources/views/doctor/datatable/action.blade.php
@@ -1,6 +1,6 @@
 <div class="btn-group" role="group">
     <div class="mx-1">
-        <button type="button" data-id="{{ $model->id }}" class="btn btn-success doctor-edit"
+        <button type="button" data-id="{{ $model->id }}" class="btn btn-sm btn-success doctor-edit"
                 data-bs-toggle="modal" data-bs-target="#editDoctorModal">
                 <i class="bi bi-pencil-square"></i> Edit
         </button>
@@ -9,7 +9,7 @@
     <div class="mx-1">
         <form action="{{ route('doctor.destroy', $model->id) }}" method="POST">
             @csrf @method('DELETE')
-            <button type="submit" class="btn btn-danger delete-dialog">
+            <button type="submit" class="btn btn-sm btn-danger delete-dialog">
                 <i class="bi bi-trash-fill"></i> Hapus
             </button>
         </form>
