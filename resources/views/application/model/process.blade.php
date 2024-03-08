e28c7d3db3cf37a109ebdb321dea4d5f482b5b38
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-03-09 00:17:09 +0800

    add: update process flow of application page

--- resources/views/application/model/process.blade.php
+++ resources/views/application/model/process.blade.php
@@ -6,6 +6,14 @@
 				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 			</div>
 			<div class="modal-body">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="mb-3">
+							<h5>Pasien</h5>
+						</div>
+					</div>
+				</div>
+
 				<form action="#" method="POST">
 					@csrf
 					<div class="row">
@@ -68,7 +76,7 @@
 
 					<div class="modal-footer">
 						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-						<button type="submit" class="btn btn-primary">Simpan</button>
+						<button type="submit" class="btn btn-primary">Simpan & Cetak</button>
 					</div>
 				</form>
 			</div>
