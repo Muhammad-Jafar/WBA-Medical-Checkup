1e5e183835a1a32f1ad912d10dc7082ec8161a4d
Jafar 131 <57802779+Muhammad-Jafar@users.noreply.github.com>
2024-04-17 00:12:07 +0800

    add: update rule for print

--- app/Helpers.php
+++ app/Helpers.php
@@ -28,3 +28,96 @@ if (!function_exists('reverseDate')) {
         return Carbon::createFromFormat('d-m-Y', $date)->format('Y-m-d');
     }
 }
+
+if(!function_exists('monthToBulan')) {
+    /**
+     * Convert the month to a string with indonesian format.
+     * 
+     * @param string $month
+     * @return string | null
+     */
+     function monthToBulan($input): string
+     {
+        $months = [
+            '01' => 'JANUARI',
+            '02' => 'FEBRUARI',
+            '03' => 'MARET',
+            '04' => 'APRIL',
+            '05' => 'MEI',
+            '06' => 'JUNI',
+            '07' => 'JULI',
+            '08' => 'AGUSTUS',
+            '09' => 'SEPTEMBER',
+            '10' => 'OKTOBER',
+            '11' => 'NOVEMBER',
+            '12' => 'DESEMBER'
+        ];
+
+        return $months[$input] ?? 'Unpexpected month';
+     } 
+}
+
+if(!function_exists('monthToFullBulan')) {
+    /**
+     * Convert the month to a string with indonesian format.
+     * 
+     * @param string $month
+     * @return string | null
+     */
+     function monthToFullBulan($input): string
+     {
+        $monthsIndonesian = [
+            'January' => 'JANUARI',
+            'February' => 'FEBRUARI',
+            'March' => 'MARET',
+            'April' => 'APRIL',
+            'May' => 'MEI',
+            'June' => 'JUNI',
+            'July' => 'JULI',
+            'August' => 'AGUSTUS',
+            'September' => 'SEPTEMBER',
+            'October' => 'OKTOBER',
+            'November' => 'NOVEMBER',
+            'December' => 'DESEMBER'
+        ];
+
+        try {
+            $date = new DateTime($input);
+            $monthEng = $date->format('F'); // Get full English month name
+            $monthInd = $monthsIndonesian[$monthEng] ?? $monthEng; // Translate month
+
+            return $date->format('j ') . $monthInd . $date->format(' Y'); // Format date as '1 April 2024'
+        } catch (Exception $e) {
+            // Handle exception if the date format is incorrect
+            return "Invalid date format";
+        }
+     } 
+}
+
+if(!function_exists('monthToBulanRomawi')) {
+    /**
+     * Convert the month to a string with indonesian format.
+     * 
+     * @param string $month
+     * @return string | null
+     */
+     function monthToBulanRomawi($input): string
+     {
+        $months = [
+            '01' => 'I',
+            '02' => 'II',
+            '03' => 'III',
+            '04' => 'IV',
+            '05' => 'V',
+            '06' => 'VI',
+            '07' => 'VII',
+            '08' => 'VIII',
+            '09' => 'IX',
+            '10' => 'X',
+            '11' => 'XI',
+            '12' => 'XII'
+        ];
+
+        return $months[$input] ?? 'Unpexpected month';
+     } 
+}
